ea2c28c9936d76623010553ee59f813df11911bf
Tonwantpillow <kerdsirija@gmail.com>
2025-10-31 23:31:48 +0700

    add cart page navigation

--- interface/components/CartBtn.tsx
+++ interface/components/CartBtn.tsx
@@ -1,8 +1,13 @@
+'use client'
+
 import { ShoppingBasket } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 export default function CartBtn() {
+  const router = useRouter();
+
   return (
-    <button className="cursor-pointer" onClick={() => {}}>
+    <button className="cursor-pointer" onClick={() => router.push('/cart')}>
       <ShoppingBasket className="text-primary-subtle size-[35px]"/>
     </button>
   );
